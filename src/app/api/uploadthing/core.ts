4aed1062c1ca4008964156734f6bfc4db11c17fd
Theo Browne <me@t3.gg>
2023-06-17 04:59:19 -0700

    block users who aren't enabled

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,29 +1,23 @@
 import { db } from "@/db";
 import { uploadedImage } from "@/db/schema";
-import { auth } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { eq } from "drizzle-orm";
 import { uploadTransparent } from "./make-transparent-file";
 
 const f = createUploadthing();
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 20 } })
-    // Set permissions and file types for this FileRoute
     .middleware(async (req) => {
-      // This code runs on your server before upload
-      const user = await auth();
+      const user = await currentUser();
 
-      // If you throw, the user will not be able to upload
-      if (!user || !user.userId) throw new Error("Unauthorized");
+      if (!user || !user.id || !user.privateMetadata.enabled)
+        throw new Error("Unauthorized");
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.userId };
+      return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
 
       console.log("file url", file.url);
