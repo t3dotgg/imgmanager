c6f24b9e847e4c4e6df1172dc0b3e9068582bfcd
Theo Browne <me@t3.gg>
2023-11-01 00:54:24 -0700

    Delete flow

--- src/app/_components/magic-image.tsx
+++ src/app/_components/magic-image.tsx
@@ -4,6 +4,9 @@ import type { ImageFromDb } from "@/db/queries";
 import NextImage from "next/image";
 import { useState } from "react";
 import { Spinner } from "./loading-spinner";
+import { useIdToggle } from "./selection/store";
+import clsx from "clsx";
+import { useAuth } from "@clerk/nextjs";
 
 async function downloadAndCopyImageToClipboard(imageUrl: string) {
   console.log("dc image", imageUrl);
@@ -44,29 +47,45 @@ async function downloadAndCopyImageToClipboard(imageUrl: string) {
 }
 
 export const MagicImageRender = ({ image }: { image: ImageFromDb }) => {
+  const { isSignedIn } = useAuth();
   // For when doing copying
   const [loading, setLoading] = useState(false);
   // For when copying is done
   const [done, setDone] = useState(false);
 
+  // For selection (for deletion)
+  const { toggle, isSelected } = useIdToggle(image.fileKey);
+
   return (
-    <div
-      className="relative flex w-full flex-col items-center justify-center hover:bg-gray-700/40 hover:opacity-80"
-      onClick={() => {
-        console.log("copying image", image);
-        setLoading(true);
-        downloadAndCopyImageToClipboard(
-          image.removedBgUrl ?? image.originalUrl ?? ""
-        ).then(() => {
-          setLoading(false);
-          setDone(true);
-          setTimeout(() => {
-            setDone(false);
-          }, 1000);
-        });
-      }}
-    >
-      <div className="relative h-48 w-full">
+    <div className="group relative flex w-full flex-col items-center justify-center hover:bg-gray-700/40 hover:opacity-80">
+      {isSignedIn && (
+        <input
+          type="checkbox"
+          className={clsx(
+            "absolute right-0 top-0 z-10 m-4 hidden h-4 w-4 group-hover:block"
+          )}
+          // Overwrite style if selected
+          style={isSelected ? { display: "block" } : {}}
+          checked={isSelected}
+          onChange={toggle}
+        />
+      )}
+      <div
+        className="relative h-48 w-full"
+        onClick={() => {
+          console.log("copying image", image);
+          setLoading(true);
+          downloadAndCopyImageToClipboard(
+            image.removedBgUrl ?? image.originalUrl ?? ""
+          ).then(() => {
+            setLoading(false);
+            setDone(true);
+            setTimeout(() => {
+              setDone(false);
+            }, 1000);
+          });
+        }}
+      >
         <NextImage
           src={image.removedBgUrl ?? image.originalUrl ?? ""}
           alt={image.originalName}
