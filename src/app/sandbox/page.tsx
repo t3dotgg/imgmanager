3df80ca5d66b3353b2b2d9ee244c05101fec2692
Theo Browne <me@t3.gg>
2024-04-08 23:30:00 -0700

    Delete sandbox code

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,50 +1,34 @@
 import { db } from "@/db";
-import { uploadTransparentAlt } from "../api/uploadthing/make-transparent-cheaper";
-import { uploadTransparent } from "../api/uploadthing/make-transparent-file";
-
 import fs from "fs/promises";
 import { uploadedImage } from "@/db/schema";
 
-export const runtime = "nodejs";
+// // Run this while on old DB
+// async function dumpImagesFromDB() {
+//   "use server";
+//   const allImages = await db.query.uploadedImage.findMany();
 
-async function doSomeWorkonServer() {
-  "use server";
+//   await fs.writeFile("db-dump.json", JSON.stringify(allImages));
+// }
 
-  const allImages = await db.query.uploadedImage.findMany();
+// // Run this while on new DB
+// async function updateDBFromDump() {
+//   "use server";
+//   const imagesFromFile = await fs.readFile("db-dump.json", "utf8");
 
-  console.log("allImages", allImages.length, allImages[0]);
-
-  // Write object to file as json
-  await fs.writeFile("uploadthing.json", JSON.stringify(allImages));
-
-  const resp = await uploadTransparent(
-    "https://uploadthing-prod.s3.us-west-2.amazonaws.com/3d55b9a4-b7e6-4db7-94b0-cf671367b454_P1000028-0054.jpg"
-  );
-
-  console.log("on server", resp);
-}
-
-async function uploadImagesFromDump() {
-  "use server";
-
-  const imagesFromFile = await fs.readFile("uploadthing.json", "utf8");
-  const images = JSON.parse(imagesFromFile);
-
-  console.log("images", images);
-
-  const addedToDb = await db.insert(uploadedImage).values(images);
-}
+//   await db.insert(uploadedImage).values(JSON.parse(imagesFromFile));
+// }
 
 export default function SandboxPage() {
   return (
     <div>
-      <form action={doSomeWorkonServer}>
+      Empty sandbox
+      {/* <form action={dumpImagesFromDB}>
         <button type="submit">Download all images</button>
       </form>
 
-      <form action={uploadImagesFromDump}>
+      <form action={updateDBFromDump}>
         <button type="submit">Upload to db from dump</button>
-      </form>
+      </form> */}
     </div>
   );
 }
